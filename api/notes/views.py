0ab5ef2b8c35a0ce432b540b49a344a5b0c7192f
umarr <umarr.developer@gmail.com>
2024-08-29 20:33:53 +0600

    fix type response model

--- api/notes/views.py
+++ api/notes/views.py
@@ -12,7 +12,7 @@ router = fastapi.APIRouter(prefix='/notes', tags=['posts'])
 
 
 @router.post('/create/',
-             response_model=list[GetNote])
+             response_model=GetNote)
 async def on_create_note(user_schema: AuthUser,
                          note_schema: CreateNote,
                          session: AsyncSession = Depends(db_tools.session_dependency)):
