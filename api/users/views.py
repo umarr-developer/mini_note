fc681c7973bb76cddaf240ab69f242e1618ef8ae
umarr <umarr.developer@gmail.com>
2024-08-29 20:29:41 +0600

    add exceptions for user views

--- api/users/views.py
+++ api/users/views.py
@@ -14,11 +14,22 @@ router = fastapi.APIRouter(prefix='/users', tags=['users'])
              status_code=fastapi.status.HTTP_201_CREATED)
 async def on_create_user(user_schema: CreateUser,
                          session: AsyncSession = Depends(db_tools.session_dependency)):
-    return await create_user(session, user_schema)
+    user = await get_user_by_username(session, user_schema.username)
+    if not user:
+        return await create_user(session, user_schema)
+    raise fastapi.HTTPException(
+        status_code=fastapi.status.HTTP_401_UNAUTHORIZED,
+        detail='this username is already taken'
+    )
 
 
 @router.post('/notes/')
 async def on_all_notes(user_schema: AuthUser,
-                  session: AsyncSession = Depends(db_tools.session_dependency)):
+                       session: AsyncSession = Depends(db_tools.session_dependency)):
     user = await get_user_by_username(session, user_schema.username)
-    return user.notes
+    if user:
+        return user.notes
+    raise fastapi.HTTPException(
+        status_code=fastapi.status.HTTP_404_NOT_FOUND,
+        detail='user not found'
+    )
