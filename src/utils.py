b9f4fd0f4e49086a60244dcff4f4e17581c2ae75
umarr <umarr.developer@gmail.com>
2024-08-29 20:17:25 +0600

    add utils.py

---
+++ src/utils.py
@@ -0,0 +1,19 @@
+import bcrypt
+
+from api.users.schemas import AuthUser
+from src.models import User
+
+
+def hash_password(password: str) -> bytes:
+    return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+
+
+def validate_password(password: str, hashed_password: bytes) -> bool:
+    return bcrypt.checkpw(
+        password=password.encode(),
+        hashed_password=hashed_password,
+    )
+
+
+def auth_user(user: User, user_schema: AuthUser) -> bool:
+    return validate_password(user_schema.password, user.password)
