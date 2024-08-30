b7e63d3bc069d766eda60299817b3f711d4c5a3f
umarr <umarr.developer@gmail.com>
2024-08-30 14:55:44 +0600

    use correct_query function to correct text title and body

--- api/notes/views.py
+++ api/notes/views.py
@@ -17,6 +17,9 @@ async def on_create_note(user_schema: AuthUser,
                          note_schema: CreateNote,
                          session: AsyncSession = Depends(db_tools.session_dependency)):
     user = await get_user_or_exc(session, user_schema)
+    note_schema.body = await correct_note(note_schema.body)
+    note_schema.title = await correct_note(note_schema.title)
+
     return await create_note(session, note_schema, user.user_id)
 
 
